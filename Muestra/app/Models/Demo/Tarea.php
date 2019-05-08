d4a55ec03e4b426f8457e9a46229050e89afe5ad
Carlos Uh <carloselvil16@gmail.com>
2019-05-08 12:26:49 -0500

    Archivos faltantes del commit anterior

---
+++ Muestra/app/Models/Demo/Tarea.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Models\Demo;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+{
+	protected $table ='pendientes';
+	protected $primaryKey='tareaid';
+	public $incrementing=true;
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'tareaid','name', 'description', 'estatus',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        
+    ];
+}
