8cdacf9bc02a328905c21918775380188c969f79
Carlos Uh <carloselvil16@gmail.com>
2019-05-08 13:10:47 -0500

    Creacion de la migracion

---
+++ Muestra/database/migrations/2019_05_08_173158_create_tarea_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTareaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendientes', function (Blueprint $table) {
+            $table->bigIncrements('tareaid');
+            $table->timestamps();
+			$table->string('name');
+			$table->string('description');
+			$table->string('estatus');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendientes');
+    }
+}
