c689c18d2e261bea87f0ce8f19a5fcbe6d8f038d
Carlos Uh <carloselvil16@gmail.com>
2019-11-07 14:03:36 -0600

    Archivo faltante. Error: partials.header not found

---
+++ Muestra/resources/views/partials/header.blade.php
@@ -0,0 +1,109 @@
+<header class="section-header">
+<section class="header-main">
+	<div class="container">
+<div class="row align-items-center">
+	<div class="col-lg-3">
+	<div class="brand-wrap">
+		<img class="logo" src="/images/logo-dark.png">
+		<h2 class="logo-text">LOGO</h2>
+	</div> <!-- brand-wrap.// -->
+	</div>
+	<div class="col-lg-6 col-sm-6">
+		<form action="#" class="search-wrap">
+			<div class="input-group">
+			    <input type="text" class="form-control" placeholder="Search">
+			    <div class="input-group-append">
+			      <button class="btn btn-primary" type="submit">
+			        <i class="fa fa-search"></i>
+			      </button>
+			    </div>
+		    </div>
+		</form> <!-- search-wrap .end// -->
+	</div> <!-- col.// -->
+	<div class="col-lg-3 col-sm-6">
+		<div class="widgets-wrap d-flex justify-content-end">
+			<div class="widget-header">
+				<a href="#" class="icontext">
+					<div class="icon-wrap icon-xs bg2 round text-secondary"><i class="fa fa-shopping-cart"></i></div>
+					<div class="text-wrap">
+						<small>Basket</small>
+						<span>3 items</span>
+					</div>
+				</a>
+			</div> <!-- widget .// -->
+			<div class="widget-header dropdown">
+				<a href="#" class="ml-3 icontext" data-toggle="dropdown" data-offset="20,10">
+					<div class="icon-wrap icon-xs bg2 round text-secondary"><i class="fa fa-user"></i></div>
+					<div class="text-wrap">
+						<small>Hello.</small>
+						<span>Login <i class="fa fa-caret-down"></i></span>
+					</div>
+				</a>
+				<div class="dropdown-menu dropdown-menu-right">
+					<form class="px-4 py-3">
+						<div class="form-group">
+						  <label>Email address</label>
+						  <input type="email" class="form-control" placeholder="email@example.com">
+						</div>
+						<div class="form-group">
+						  <label>Password</label>
+						  <input type="password" class="form-control" placeholder="Password">
+						</div>
+						<button type="submit" class="btn btn-primary">Sign in</button>
+						</form>
+						<hr class="dropdown-divider">
+						<a class="dropdown-item" href="#">Have account? Sign up</a>
+						<a class="dropdown-item" href="#">Forgot password?</a>
+				</div> <!--  dropdown-menu .// -->
+			</div> <!-- widget  dropdown.// -->
+		</div>	<!-- widgets-wrap.// -->	
+	</div> <!-- col.// -->
+</div> <!-- row.// -->
+	</div> <!-- container.// -->
+</section> <!-- header-main .// -->
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+  <div class="container">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="main_nav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link pl-0" href="#"> <strong>All category</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Fashion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Supermarket</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Electronics</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Baby &amp Toys</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Fitness sport</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown07">
+            <a class="dropdown-item" href="#">Foods and Drink</a>
+            <a class="dropdown-item" href="#">Home interior</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Category 1</a>
+            <a class="dropdown-item" href="#">Category 2</a>
+            <a class="dropdown-item" href="#">Category 3</a>
+          </div>
+        </li>
+      </ul>
+    </div> <!-- collapse .// -->
+  </div> <!-- container .// -->
+</nav>
+
+</header> <!-- section-header.// -->
